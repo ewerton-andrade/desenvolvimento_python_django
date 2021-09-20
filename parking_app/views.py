5d6e203e38091f092eb767876ae84c892e2c174e
ewerton-andrade <ewerton-andrade@users.noreply.github.com>
2021-09-19 23:40:54 -0400

    Parte 4
    Ajustados os links da index, do logo através da template tag url; Criado o base.html para usar as mesmas partes do HTML em diferentes páginas da aplicação;  Evitei código duplicado do menu e do footer criando e incluindo partials.

--- parking_app/views.py
+++ parking_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html') #Funcao comando para mostrar na aplicacao criada com conteudo html para formatacao da pagina escrito no arquivo index.html na pasta templates dentro da aplicacao parking_app
+
+def carro(request):
+    return render(request, 'carro.html')
