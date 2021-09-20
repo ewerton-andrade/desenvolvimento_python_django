6b2b7d0977c511c0cc503dcd8c7446df50abd1d9
ewerton-andrade <ewerton-andrade@users.noreply.github.com>
2021-09-20 01:49:34 -0400

    Parte 5
    Enviei os dados para o template renderizar; Instalei e configurei o banco de dados Postgresql com a aplicação; Criei o modelo de receitas e mapeei para uma tabela no banco de dados.

--- parking_app/models.py
+++ parking_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Carro(models.Model):
+    placa_carro = models.CharField(max_length=9)
+    modelo_carro = models.CharField(max_length=100)
+    cor_do_carro = models.CharField(max_length=100)
+    entrada = models.DateTimeField(default = datetime.now)
+    saida = models.DateTimeField(default= datetime.now)
+    parking_time = models.DurationField()
+    preco = models.FloatField()
--- parking_app/views.py
+++ parking_app/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html') #Funcao comando para mostrar na aplicacao criada com conteudo html para formatacao da pagina escrito no arquivo index.html na pasta templates dentro da aplicacao parking_app
+    placas = {
+        1: 'VAP-1234',
+        2: 'TIC-8500',
+        3: 'EAD-4567',
+        4: 'REC-1234'
+    }
+
+    dados = {
+        'placa_do_carro': placas
+    }
+    return render(request,'index.html', dados) #Funcao comando para mostrar na aplicacao criada com conteudo html para formatacao da pagina escrito no arquivo index.html na pasta templates dentro da aplicacao parking_app
 
 def carro(request):
     return render(request, 'carro.html')
